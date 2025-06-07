bf5bf6e1c53115e833238c4409ddba3d436d0e69
john-philipp <john-philipp@users.noreply.github.com>
2025-06-07 11:29:17 +0200

    (#34, #38): Derive nn-relu vectorised equations.

--- readme.md
+++ readme.md
@@ -9,9 +9,9 @@ Houses some machine learning and neural network implementations. Due to the rest
 ### Logistic regression
 As a basic example I've implemented a logistic regression scheme using a sigmoid function and the gradient descent method in both numpy and pytorch. The code is maths heavy. Below is a complete derivation of the single sample case. As well as its vectorised extension actually implemented. We vectorise to reduce computing times by leveraging low-level optimised matrix multiplications over explicit top-level for-loops.
 
-![Full logistic regression derivation.](docs/images/log-reg-20250603_132609.png)
+![Full logistic regression derivation.](docs/images/log-reg-20250607_110749.png)
 
 ### Hidden layer using ReLU
 We extend the logistic regression case using a hidden layer based on the ReLU function.
 
-![Full derivation for ReLU hidden layer.](docs/images/nn-relu-20250603_154344.png)
+![Full derivation for ReLU hidden layer.](docs/images/nn-relu-20250607_112416.png)
--- scripts/make_tex_png.sh
+++ scripts/make_tex_png.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+logfile=$(mktemp)
+function _log() {
+  echo $@ | tee -a ${logfile}
+}
+
+_log [INFO] Will write logs here: ${logfile} ...
+
 name=${1:-log-reg}
 dpi=${2:-100}
 border=${3:-50}
@@ -11,39 +19,40 @@ dest_dir=docs/images
 dest_file=${dest_dir}/${name}-${timestamp}.png
 
 if [[ ! -d docs ]]; then
-  echo [ERROR] Run from root dir.
+  _log [ERROR] Run from root dir.
   exit 1
 fi
 
 if [[ ! -f ${src_file} ]]; then
-  echo [ERROR] No such file: ${src_file}.
+  _log [ERROR] No such file: ${src_file}.
   exit 1
 fi
 
 set -e
 rm -rf .tmp
 mkdir -p .tmp docs/images
-rm docs/images/${name}*.png || echo [INFO] Nothing to remove.
+rm docs/images/${name}*.png || _log [INFO] Nothing to remove.
 cd .tmp
 
-echo [INFO] Generating dvi: name=${name} ...
+_log [INFO] Generating dvi: name=${name} ...
 # First run to generate references. Second to use them.
-latex ../docs/tex/${name}.tex &> /dev/null
-latex ../docs/tex/${name}.tex &> /dev/null
+latex ../docs/tex/${name}.tex >> ${logfile}
+latex ../docs/tex/${name}.tex >> ${logfile}
 
-echo [INFO] Generating png: name=${name} dpi=${dpi} ...
-dvipng -D ${dpi} ${name}.dvi -o ${name}.png &> /dev/null
+_log [INFO] Generating png: name=${name} dpi=${dpi} ...
+dvipng -D ${dpi} ${name}.dvi -o ${name}.png >> ${logfile} 2>&1
 
-echo [INFO] Running conversion: name=${name} border=${border} ...
-convert ${name}.png -bordercolor white -border ${border}x${border} ../${dest_file} &> /dev/null
+_log [INFO] Running conversion: name=${name} border=${border} ...
+convert ${name}.png -bordercolor white -border ${border}x${border} ../${dest_file} >> ${logfile}
 
 cd ..
 rm -rf .tmp
 
-echo [INFO] Adding to index: ${dest_file} ...
+_log [INFO] Adding to index: ${dest_file} ...
 git add ${dest_file}
 
-echo [INFO] Updating readme ...
+_log [INFO] Updating readme ...
 sed -i "s|${dest_dir}/${name}-.*\.png|${dest_file}|" readme.md
 
-echo [INFO] Done.
+_log [INFO] Done.
+_log [INFO] Logs written here: ${logfile} ...
